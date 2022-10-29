10eb2c10dcf9398b168f0c14061b8fad1018587e
SasiVardhan247 <83742554+SasiVardhan247@users.noreply.github.com>
2022-10-29 15:58:12 +0530

    login prototype

---
+++ server.js
@@ -0,0 +1,26 @@
+const express=require('express');
+const dotenv =require('dotenv');
+const morgan=require('morgan');
+const bodyparser=require('body-parser');
+const path=require('path');
+const app=express();
+dotenv.config({path:'config.env'})
+const PORT=process.env.PORT||8080
+
+//log request
+app.use(morgan('tiny'));
+
+app.use(bodyparser.urlencoded({extended:true}))
+
+app.set("view engine","ejs")
+//app.set("views",path.resolve(__dirname,"views/ejs"))
+
+app.use('/css',express.static(path.resolve(__dirname,"assests/css")))
+app.use('/img',express.static(path.resolve(__dirname,"assests/img")))
+
+app.get('/',(req,res)=>{
+    // res.send("d-voting-app");
+    res.render("login_page.ejs");
+})
+
+app.listen(PORT,()=>{console.log(`Server is running on https://localhost:${PORT}`)});
